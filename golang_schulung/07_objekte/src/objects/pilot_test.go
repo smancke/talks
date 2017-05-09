0c0a098f8eac9e9dcedfd3fd10daa448df3fcc6e
Sebastian Mancke <s.mancke@tarent.de>
2017-05-09 15:07:46 +0200

    use go:generate for mock generation

--- golang_schulung/07_objekte/src/objects/pilot_test.go
+++ golang_schulung/07_objekte/src/objects/pilot_test.go
@@ -6,6 +6,8 @@ import (
 	"testing"
 )
 
+//go:generate go get github.com/golang/mock/mockgen
+//go:generate $GOPATH/bin/mockgen -self_package objects -package objects -destination $GOPATH/src/objects/mocks_test.go objects Flyable
 func Test_flyToMiddleOfUniverse_WithoutMock(t *testing.T) {
 	herzAusGold := NewStarship("HerzAusGold", 42)
 
